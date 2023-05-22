dbb6ff650114cf2bb8bfa9cef4bc3a1f577781b1
Nezir Erdinc <90757456+nezirer@users.noreply.github.com>
2023-05-22 23:37:38 +0300

    Arkadas sayı mi degil mi ?.java
            /* ARKADAŞ SAYILAR => KENDİLERİ HARİÇ POZİTİF TAM SAYILARININ TOPLAMI DİĞER SAYIYI VERİYORSA */

---
+++ Arkadas sayı mi degil mi ?.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /* ARKADAŞ SAYILAR => KENDİLERİ HARİÇ POZİTİF TAM SAYILARININ TOPLAMI DİĞER SAYIYI VERİYORSA */
+
+
+        int sayi1 = 220;
+        int sayi2 = 284 ;
+        int toplam1 = 0;
+        int toplam2 = 0;
+
+
+        for (int i = 1 ; i < sayi1;i++){
+
+            if (sayi1 % i == 0){
+                toplam1 =toplam1 + i;
+
+            }
+
+        }
+
+        for (int i = 1 ; i < sayi2;i++){
+
+
+            if (sayi2 % i == 0){
+                toplam2 = toplam2 + i;
+
+            }
+
+        }
+
+        if (toplam1 == sayi2 || toplam2 == sayi1) {
+            System.out.println("Sayılar arkdaş sayılardır.");
+        }
+
+        else
+            System.out.println("Sayılar arkadaş sayı değildir.");
+    }
+
+}
